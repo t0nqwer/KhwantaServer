9d24583f2d1ff9ba026857ecd9ea22776f50efb3
t0nqweer <oottoonnoo@gmail.com>
2023-11-01 01:34:47 +0700

    master

--- controllers/lalynnWebsite/fetchdata.js
+++ controllers/lalynnWebsite/fetchdata.js
@@ -1,8 +1,11 @@
 import Product from "../../models/product.js";
+import Design from "../../models/design.js";
 
 export const fetchHeroProduct = async (req, res) => {
   try {
-    const products = await Product.find().limit(6);
+    const design = await Design.find({ brand: "Lalynn" }).select("_id");
+    const idarray = design.map((item) => item._id);
+    const products = await Product.find({ design: { $in: idarray } }).limit(6);
     res.status(200).json(products);
   } catch (error) {
     res.status(404).json({ message: error.message });
