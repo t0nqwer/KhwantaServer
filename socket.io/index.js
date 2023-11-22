ee4634a9c96c59a7db6dc42dbd51af8cfb0bd0c2
t0nqwer <oottoonnoo@gmail.com>
2023-11-22 14:41:25 +0700

    master

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,16 +1,16 @@
-import { Server } from "socket.io";
-import { server } from "../index.js";
+// import { Server } from "socket.io";
+
 const PORT = 7070;
-export let io = new Server(PORT);
-io.close();
+// export let io = new Server(PORT);
+// io.close();
 
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
-  // socket.emit("newProduct", "data");
+// io.on("connection", (socket) => {
+//   console.log("a user connected", socket.id);
+//   // socket.emit("newProduct", "data");
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
 
-export const NotifyNewProduct = (data) => io.broadcast.emit("newProduct", data);
+// export const NotifyNewProduct = (data) => io.broadcast.emit("newProduct", data);
