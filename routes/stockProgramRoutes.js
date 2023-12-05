6bf1fbd32a6679ca0425e73f97931bb12f6ea51c
t0nqweer <oottoonnoo@gmail.com>
2023-12-05 13:31:32 +0700

    master

--- routes/stockProgramRoutes.js
+++ routes/stockProgramRoutes.js
@@ -5,7 +5,7 @@ import { NewTransfer } from "../controllers/stockProgram/Transfer.js";
 import { AlertNewTransfer } from "../socket.io/Func.js";
 const router = express.Router();
 
-router.get("/", startApp);
+router.get("/startApp", startApp);
 router.post("/startApp", startApp);
 router.post("/stockin", StockIn, updateStock);
 router.post("/transfer", NewTransfer, AlertNewTransfer);
